7bb77f2dfe15822fca8275067307bb8f61bd5937
jeancdev <jcode2006@gmail.com>
2025-09-02 21:23:22 -0500

    feat: add external port variables for Redis, Postgres, and Grafana

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "nginx_external_port" {
   description = "Coloca el puerto de Nginx"
 }
+
+variable "redis_external_port" {
+  description = "Coloca el puerto de Redis"
+}
+
+variable "postgres_external_port" {
+  description = "Coloca el puerto de Postgres"
+}
+
+variable "grafana_external_port" {
+  description = "Coloca el puerto de Grafana"
+}
