f6878d7434a07526a17e135586ddb416fe91fc8e
Jeancarlo Morales <72767265+jeancdevx@users.noreply.github.com>
2025-09-03 03:47:23 +0000

    feat: update docker network names

--- network.tf
+++ network.tf
@@ -1,11 +1,11 @@
 resource "docker_network" "app_net" {
-  name  = "app_net"
+  name  = "app_net_${terraform.workspace}"
 }
 
 resource "docker_network" "persistence_net" {
-  name  = "persistence_net"
+  name  = "persistence_net_${terraform.workspace}"
 }
 
 resource "docker_network" "monitor_net" {
-  name  = "monitor_net"
+  name  = "monitor_net_${terraform.workspace}"
 }
